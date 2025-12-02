9472d668dc9a0ff6898e84b269d06999f8c55cfd
Dan Elvey <dan.elvey@trustedhousesitters.com>
2025-12-02 09:19:12 +0000

    day 1 part 2 - part 2 successful on sample but not full input

--- solutions/day01/part2.py
+++ solutions/day01/part2.py
@@ -4,27 +4,31 @@ def move_dial(old_position, line):
     direction = line[0]
     steps = int(line[1:])
     bonus_zero_steps = 0
-    wrap_arounds = 0
-
-    if steps > 99:
-        wrap_arounds = str(steps)[0]
 
     if direction == "R":
         new_position = (old_position + steps) % 100
-    else: 
+        bonus_zero_steps = (old_position + steps) // 100
+        # print(f"Calculating bonus zeros going R: {old_position} + {steps} // 100 = {bonus_zero_steps} - landed on {new_position}")
+    else:
         new_position = (old_position - steps) % 100
+        bonus_zero_steps = ((old_position - steps) // 100) * -1
+        # print(f"Calculating bonus zeros going L: {old_position} - {steps} // 100 = {bonus_zero_steps} - landed on {new_position}")
 
-    return new_position
+    if new_position == 0:
+        bonus_zero_steps -= 1
+
+    return new_position, bonus_zero_steps
 
 def solve(input_text):
-    """Solve part 1 of the puzzle."""
+    """Solve part 2 of the puzzle."""
 
     lines = input_text.strip().split('\n')
     position = 50
     zero_count = 0
 
     for line in lines:
-        position = move_dial(position, line)
+        position, bonus_zero_steps = move_dial(position, line)
+        zero_count += bonus_zero_steps
         if position == 0:
             zero_count += 1
         
