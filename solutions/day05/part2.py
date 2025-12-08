ed56e00f043017ed85007807dcaaf779a2c1019c
Dan Elvey <dan.elvey@trustedhousesitters.com>
2025-12-08 17:45:25 +0000

    file tidy

--- solutions/day05/part2.py
+++ solutions/day05/part2.py
@@ -1,16 +1,33 @@
 from pathlib import Path
 
+
 def solve(input_text):
-    """Solve part 2 of the puzzle."""
-    lines = input_text.strip().split('\n')
-    
-    # TODO: Implement solution
-    
-    return None
+    """Solve part 1 of the puzzle."""
+    lines = input_text.strip().split("\n")
+    split_index = lines.index("")
+    ranges = lines[:split_index]
+    fresh_ids = set()
+
+    """
+        The ranges are all we care about. I want to know the numbers in between those ranges. 
+        Don't try to generate all the numbers in between because this will crash the machine.
+        Just add what the difference is in the ranges and then we can add all of those numbers together to get the total count of fresh IDs.
+
+        I need to factor in duplicates in the ranges
+    """
+
+    for id_range in ranges:  # This is a solution that generates all the ids - this will use a tonne of memory
+        start, end = map(int, id_range.split("-"))
+        difference = end - start
+        for i in range(difference + 1):
+            fresh_ids.add(start + i)
+
+    return len(fresh_ids)
+
 
 if __name__ == "__main__":
-    input_file = Path(__file__).parent.parent.parent / "inputs" / "day05.txt"
+    input_file = Path(__file__).parent.parent.parent / "inputs" / "day05-sample.txt"
     input_text = input_file.read_text()
-    
+
     result = solve(input_text)
     print(f"Answer: {result}")
