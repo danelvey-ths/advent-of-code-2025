4b37706b0d9ddd02cd4f227b086f0a41d79b5282
Dan Elvey <dan.elvey@trustedhousesitters.com>
2025-12-09 17:54:14 +0000

    day 6 part 2 - looks good but bad doing sums based on operator changing

--- solutions/day06/part2.py
+++ solutions/day06/part2.py
@@ -5,9 +5,8 @@ from functools import reduce
 def solve(input_text):
     """Solve part 2 of the puzzle."""
     lines = input_text.strip().split("\n")
-    stripped_lines = []
     result = 0
-    split_reversed_lines = []
+    split_lines = []
 
     """
         The last line is always the operation to be performed.
@@ -24,44 +23,65 @@ def solve(input_text):
                               356 * 24 * 1 = 8544
                               sums to 3263827
     """
+
+    # This splits each number into its own list item
     for line in lines:
-        split_reversed = []
+        split = []
         line = list(line.split(" "))
         print(line)
         for num in line:
-            if num == "+" or num == "*" and len(split_reversed) < 1:
-                split_reversed.insert(0, "")
-            if not num.isdigit():
-                split_reversed.append(num)
+            if num == "":  # Check for spaces, add these in between numbers too
+                split.append(" ")
+                continue
+            if not num.isdigit():  # Puts individual digits into the list
+                split.append(num)
                 continue
 
             num = [n for n in str(num)]
-            num = list(reversed(num))
             for n in num:
-                split_reversed.append(n)
-            # print(num)
+                split.append(n)
 
-        split_reversed_lines.append(split_reversed)
+        split_lines.append(split)
 
-    for line in split_reversed_lines:
-        print(len(line), line)
+    for i in range(len(split_lines[-1])):
+        if split_lines[-1][i] == " ":
+            split_lines[-1][i] = split_lines[-1][i - 1]
+        if len((split_lines[-1])) < len(split_lines[0]):
+            difference = len(split_lines[0]) - len(split_lines[-1])
+            for _ in range(difference):
+                split_lines[-1].append(
+                    split_lines[-1][-1]
+                )  # Extends the last element to match length with the other rows
+    # for line in split_lines:
+    #     print(len(line), line)
 
-    rotated_lines = list(zip(*stripped_lines[::-1]))
+    rotated_lines = list(zip(*split_lines[::1]))
 
-    # for line in rotated_lines:
-    #     operator = line[0]
-    #     numbers = tuple(map(int, line[1:]))
+    print()
+    numbers_to_sum = []
+    for i in range(len(rotated_lines)):
+        col = rotated_lines[i]
+        print(col)
+        current_operator = col[-1]
+        next_operator = rotated_lines[i + 1][-1] if i + 1 < len(rotated_lines) else None
 
-    #     if operator == "+":
-    #         result += sum(numbers)
-    #     elif operator == "*":
-    #         result += reduce(lambda x, y: x * y, numbers)
-
+        numbers = [int(n) for n in col[:-1] if n != " "]
+        numbers = "".join(str(number) for number in numbers)
+        numbers_to_sum.append(int(numbers))
+        if next_operator != current_operator:
+            if current_operator == "+":
+                print(numbers_to_sum, current_operator)
+                result += sum(numbers_to_sum)
+                numbers_to_sum = []
+            elif current_operator == "*":
+                print(numbers_to_sum, current_operator)
+                result += reduce(lambda x, y: x * y, numbers_to_sum)
+                numbers_to_sum = []
     return result
 
 
 if __name__ == "__main__":
-    input_file = Path(__file__).parent.parent.parent / "inputs" / "day06-sample.txt"
+    input_file = Path(__file__).parent.parent.parent / "inputs" / "day06.txt"
     input_text = input_file.read_text()
 
     result = solve(input_text)
