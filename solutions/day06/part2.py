31b4167babc53b4fd9ac755de27f694b0b27791a
Dan Elvey <dan.elvey@trustedhousesitters.com>
2025-12-09 21:37:37 +0000

    day 6 part 2

--- solutions/day06/part2.py
+++ solutions/day06/part2.py
@@ -8,27 +8,11 @@ def solve(input_text):
     result = 0
     split_lines = []
 
-    """
-        The last line is always the operation to be performed.
-        The last line always has the operator at the start, then the amount of spaces between it and the next one is the "width" of the column
-        We need to reverse the lines
-        123 328  51  64 
-         45 64  387  23 
-          6 98  215 314
-        *   +   *   +
-
-        right to left becomes 4 + 431 + 623 = 1058
-                              175 * 581 * 32 = 3253600
-                              8 + 248 + 369 = 625
-                              356 * 24 * 1 = 8544
-                              sums to 3263827
-    """
-
-    # This splits each number into its own list item
+    # Splits each number into its own list item
     for line in lines:
         split = []
         line = list(line.split(" "))
-        print(line)
+
         for num in line:
             if num == "":  # Check for spaces, add these in between numbers too
                 split.append(" ")
@@ -37,6 +21,7 @@ def solve(input_text):
                 split.append(num)
                 continue
 
+            # Breaks down multi-digit numbers into individual digits
             num = [n for n in str(num)]
             for n in num:
                 split.append(n)
@@ -44,39 +29,54 @@ def solve(input_text):
         split_lines.append(split)
 
     for i in range(len(split_lines[-1])):
-        if split_lines[-1][i] == " ":
-            split_lines[-1][i] = split_lines[-1][i - 1]
         if len((split_lines[-1])) < len(split_lines[0]):
             difference = len(split_lines[0]) - len(split_lines[-1])
             for _ in range(difference):
-                split_lines[-1].append(
-                    split_lines[-1][-1]
-                )  # Extends the last element to match length with the other rows
-    # for line in split_lines:
-    #     print(len(line), line)
+                # Extends the last element to match length with the other rows
+                split_lines[-1].append(" ")
 
+    # Rotates the rows into columns so they can be processed. This also puts the operator first in each column so it's easier to predict when to perform operations
     rotated_lines = list(zip(*split_lines[::1]))
 
-    print()
     numbers_to_sum = []
+    current_operator = " "
+
     for i in range(len(rotated_lines)):
         col = rotated_lines[i]
-        print(col)
-        current_operator = col[-1]
-        next_operator = rotated_lines[i + 1][-1] if i + 1 < len(rotated_lines) else None
+        if col[-1] == " ":
+            pass
+        else:
+            current_operator = col[-1]
 
+        # Strip the numbers from the column list and reform them back into numbers to be calculated
         numbers = [int(n) for n in col[:-1] if n != " "]
         numbers = "".join(str(number) for number in numbers)
         numbers_to_sum.append(int(numbers))
-        if next_operator != current_operator:
+        # Gather these numbers and perform the operation one ahead of the next operator change
+
+        # Check if next column is an operator change or end of list
+        if i + 1 < len(rotated_lines) and (
+            rotated_lines[i + 1][-1] == "+" or rotated_lines[i + 1][-1] == "*"
+        ):
             if current_operator == "+":
-                print(numbers_to_sum, current_operator)
-                result += sum(numbers_to_sum)
+                line_operation = sum(numbers_to_sum)
+                result += line_operation
                 numbers_to_sum = []
+                continue
             elif current_operator == "*":
-                print(numbers_to_sum, current_operator)
-                result += reduce(lambda x, y: x * y, numbers_to_sum)
+                line_operation = reduce(lambda x, y: x * y, numbers_to_sum)
+                result += line_operation
                 numbers_to_sum = []
+                continue
+    # Final operation after the loop ends
+    if numbers_to_sum:
+        if current_operator == "+":
+            line_operation = sum(numbers_to_sum)
+            result += line_operation
+        elif current_operator == "*":
+            line_operation = reduce(lambda x, y: x * y, numbers_to_sum)
+            result += line_operation
+
     return result
 
 
